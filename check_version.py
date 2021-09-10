92cc66234c4f9048903fc3daf8d9fbb93cf92291
Haoyuan Li <haoyuan8@ualberta.ca>
2021-09-10 14:12:10 -0600

    add URL

--- check_version.py
+++ check_version.py
@@ -3,7 +3,7 @@ import inspect
 
 
 def get_file(filename="QUESTION8.py"):
-    URL=None
+    URL="https://github.com/Amos-lii/404/blob/main/check_version.py"
     down = requests.get(URL)
     with open(filename, "wb") as file:
         file.write(down.content
