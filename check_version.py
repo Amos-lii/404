877d067c372e1347e3be57ac1f7675c279cca047
Haoyuan Li <haoyuan8@ualberta.ca>
2021-09-10 14:52:53 -0600

    dubug

--- check_version.py
+++ check_version.py
@@ -1,8 +1,11 @@
 import requests
 
 
-URL="https://github.com/Amos-lii/404/blob/main/check_version.py"
-res=requests.get(URL)
-print(res)
-sourcecode=requests.get("https://github.com/Amos-lii/404/blob/main/check_version.py")
+def get_file(filename="QUESTION8.py"):
+    URL="https://raw.githubusercontent.com/Amos-lii/404/main/check_version.py"
+    down = requests.get(URL)
+    with open(filename, "wb") as file:
+        file.write(down.content
+                   )
+sourcecode=requests.get("https://raw.githubusercontent.com/Amos-lii/404/main/check_version.py")
 print(sourcecode)
